57b71925b2b56247d397b0eea2e6bfa1f8679c04
Davidkc85 <charixx85@gmail.com>
2022-09-14 12:49:55 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,6 +8,7 @@
 void print_alphabet_x10(void)
 {
 	int i = 0;
+
 	while (i < 10)
 {
 	char alphabet = 'a';
@@ -15,8 +16,7 @@ void print_alphabet_x10(void)
 	while (alphabet <= 'z')
 {
 	_putchar(alphabet);
-	alphabet++;
-	
+	alphabet++;	
 }
 _putchar('\n');
 i++;
