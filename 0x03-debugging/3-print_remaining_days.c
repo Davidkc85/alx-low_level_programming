71cf2310201798b863de3d16cddd911f20fe382e
Davidkc85 <charixx85@gmail.com>
2022-09-15 17:25:53 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,13 +11,13 @@
 
 void print_remaining_day(int month, int day, int year)
 {
-if (year % 400 -- 0) || (year % 4 -- 0 && year % 100 != 0)
+if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 if (month >= 3 && day >= 60)
 {
 day++;
 }
 
-printf(*Day of the year: %d\n", day);
+printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
 else
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,8 +1,1 @@
-
-0x03. c -Debbugging
-
-by David Akuta
-
-ongoing project - started 08-31-2121
-
-checkers was released at 09-01-2121 pm
+0x03-debugging
