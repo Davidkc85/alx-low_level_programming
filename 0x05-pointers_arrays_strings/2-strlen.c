cc78ddd89bb27772d017bba02ff026cb525ec1a6
Davidkc85 <charixx85@gmail.com>
2022-09-22 00:30:18 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - function for counting the lenght of string
+ * @s: a pointer for counting the string lenght
+ * Author: AkutaDavid
+ * Return: int i
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		!++;
+	}
+	return (i);
+}
