2c4106cda9c9ad1cf6f7b6747d5b6beff109888c
Davidkc85 <charixx85@gmail.com>
2022-09-22 01:32:34 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,9 +1,10 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
  * _puts - prints a string to stdout
  * @str: pointer parameter
- * Authur: AkutaDavid
- * Return: int i
+ * Authur: AkutaDavi
  */
 
 void _puts(char *str)
@@ -14,9 +15,6 @@ void _puts(char *str)
 	{
 		_putchar(str[i]);
 		i++
-		{
-			_putchar('\n');
-		}
-	
-	return (i);
+	}
+	_putchar('\n');
 }
